e98720f07bef52a4456a5346e972377c7fbc9648
superman <chc1006@163.com>
2016-04-06 19:06:34 +0800

    'init'

---
+++ character-01/demo.js
@@ -0,0 +1,5 @@
+/**
+ * Created by superman on 2016/4/6.
+ */
+
+console.log('1');
---
+++ character-01/let.js
@@ -0,0 +1,10 @@
+/**
+ * Created by superman on 2016/4/6.
+ */
+'use strict';
+{
+    let a = 1;
+    var b = 2;
+}
+console.log(b); 
+console.log(a);
